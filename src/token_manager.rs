c6befee8ea26895a515cb5f60c2cf5dab19f71c7
kody.low <kodylow7@gmail.com>
2023-10-03 22:41:19 -0700

    replit keygen

--- src/token_manager.rs
+++ src/token_manager.rs
@@ -1,5 +1,8 @@
 use std::env;
 
+use serde::Deserialize;
+use std::process::Command;
+
 // Token manager trait and implementations
 pub trait TokenManager {
     fn get_token(&self) -> String;
@@ -9,7 +12,9 @@ pub trait TokenManager {
 pub struct ReplitIdentityTokenManager;
 
 impl TokenManager for ReplitIdentityTokenManager {
-    fn get_token(&self) -> String {}
+    fn get_token(&self) -> String {
+        generate_replit_key()
+    }
 
     fn get_token_type(&self) -> String {
         "Bearer".to_string()
@@ -26,4 +31,36 @@ impl TokenManager for L402TokenManager {
     fn get_token_type(&self) -> String {
         "L402".to_string()
     }
+}
+
+#[derive(Deserialize)]
+pub struct ReplitTokenManagerResponse {
+    pub token: String,
+    pub timeout: i64,
+}
+
+pub fn generate_replit_key() -> String {
+    println!("Replit Dynamic API Key ...");
+    let repl_slug = env::var("REPL_SLUG").expect("REPL_SLUG not set");
+    let script_path = format!("/home/runner/{}/replit/get_token.py", repl_slug);
+
+    let proc = Command::new("python")
+        .arg(script_path)
+        .output()
+        .expect("Failed to execute Get Replit API KEY process");
+    let proc_stdout = String::from_utf8_lossy(&proc.stdout);
+
+    if proc_stdout.is_empty() {
+        return "".to_string();
+    }
+
+    let proc_stdout = proc_stdout.trim();
+
+    // Parse the output into the ReplitTokenManagerResponse struct
+    let res: ReplitTokenManagerResponse =
+        serde_json::from_str(&proc_stdout).expect("Failed to parse JSON");
+
+    println!("Generated Key!");
+
+    res.token
 }
